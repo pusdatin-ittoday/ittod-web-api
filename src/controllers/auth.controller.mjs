2db79cb3a48a111c923f4353e1a85d50afa3bfd3
Aldi Pramudya <llenndesu@gmail.com>
2025-04-26 00:58:23 +0700

    feat: auth controllers

---
+++ src/controllers/auth.controller.mjs
@@ -0,0 +1,23 @@
+import * as authService from "../services/auth.service.mjs";
+
+export const register = async (req, res) => {
+    try {
+        const result = await authService.register(req.body);
+        res.status(201).json(result);
+    } catch (err) {
+        res.status(err.status || 500).json({ error: err.message });
+    }
+};
+
+export const verifyEmail = async (req, res) => {
+    try {
+        const result = await authService.verifyEmail(req.query.token);
+        res.send(result.message);
+    } catch (err) {
+        res.status(err.status || 500).json({ error: err.message });
+    }
+};
+
+export const login = (req,res) => {
+    res.json({message: "Login successful", user:req.user})
+}
