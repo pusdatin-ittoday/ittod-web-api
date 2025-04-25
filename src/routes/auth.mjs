548279dd18517169550f1d4bdf9e4d884f30e21f
Aldi Pramudya <llenndesu@gmail.com>
2025-04-25 22:43:08 +0700

    feat: added endpoint /api/auth/status

--- src/routes/auth.mjs
+++ src/routes/auth.mjs
@@ -8,30 +8,43 @@ import googleStrategy from "../strategies/google-strategy.mjs";
 
 const authRouter = Router();
 
-authRouter.post("/api/auth/login", preventLoginIfAuthenticated, validateLogin, (req, res) => {
-  res.status(200).send("Login successful");
-});
-
-authRouter.get(
-  "/api/auth/google",
-  passport.authenticate("google", { scope: ["profile", "email"] })
+authRouter.post(
+    "/api/auth/login",
+    preventLoginIfAuthenticated,
+    validateLogin,
+    (req, res) => {
+        res.status(200).send("Login successful");
+    }
 );
 
 authRouter.get(
-  "/api/auth/google/redirect",
-  passport.authenticate("google", { failureRedirect: "/login" }),
-  (req, res) => {
-    res.redirect("/");
-  }
+    "/api/auth/google",
+    passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+authRouter.get(
+    "/api/auth/google/redirect",
+    passport.authenticate("google", { failureRedirect: "/login" }),
+    (req, res) => {
+        res.redirect("/");
+    }
 );
 
 authRouter.get("/logout", (req, res) => {
-  req.logout(err => {
-    if (err) {
-      return res.status(500).send("Error logging out.");
+    req.logout(err => {
+        if (err) {
+            return res.status(500).send("Error logging out.");
+        }
+        res.redirect("/");
+    });
+});
+
+authRouter.get("/api/auth/status", (req, res) => {
+    if (req.isAuthenticated()) {
+        res.status(200).json({ authenticated: true, user: req.user });
+    } else {
+        res.status(200).json({ authenticated: false });
     }
-    res.redirect("/");
-  });
 });
 
 export default authRouter;
