c4cd8d751fbe7e9cdce23c2071472c95e4280031
Aldi Pramudya <llenndesu@gmail.com>
2025-04-28 10:05:52 +0700

    style: removed angle brackets

--- src/config/mailer.config.mjs
+++ src/config/mailer.config.mjs
@@ -1,8 +1,8 @@
-export const mailerConfig = [{
+export const mailerConfig = {
     host: "smtp.ethereal.email",
     port: 587,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
-}]; //will be used later
+}; //will be used later
