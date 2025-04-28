afad2eeabcc91c3139062caee980ea958b70137d
Aldi Pramudya <llenndesu@gmail.com>
2025-04-28 10:06:14 +0700

    chore: changed password and full name requirements

--- src/validators/registerValidationSchema.mjs
+++ src/validators/registerValidationSchema.mjs
@@ -5,11 +5,11 @@ const registerSchema = Joi.object({
         "string.email": "A valid email is required.",
         "any.required": "Email is required.",
     }),
-    password: Joi.string().min(6).required().messages({
-        "string.min": "Password must be at least 6 characters long.",
+    password: Joi.string().min(8).required().messages({
+        "string.min": "Password must be at least 8 characters long.",
         "any.required": "Password is required.",
     }),
-    full_name: Joi.string().min(6).required().messages({
+    full_name: Joi.string().min(2).required().messages({
         "string.min": "Full name must be at least 6 characters long.",
         "any.required": "Full name is required.",
     })
