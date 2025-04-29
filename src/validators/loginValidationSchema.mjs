e6b1fb1c36e009f1d6d81abc720864cbdd469eaf
Aldi Pramudya <llenndesu@gmail.com>
2025-04-29 22:37:51 +0700

    fix: mending password minimal 8 dah

--- src/validators/loginValidationSchema.mjs
+++ src/validators/loginValidationSchema.mjs
@@ -5,8 +5,8 @@ const loginSchema = Joi.object({
         "string.email": "A valid email is required.",
         "any.required": "Email is required.",
     }),
-    password: Joi.string().min(6).required().messages({
-        "string.min": "Password must be at least 6 characters long.",
+    password: Joi.string().min(8).required().messages({
+        "string.min": "Password must be at least 8 characters long.",
         "any.required": "Password is required.",
     }),
 });
