6c52942d192f808e0a946a2d7f77c7d966f77cbc
Aldi Pramudya <llenndesu@gmail.com>
2025-04-30 20:33:57 +0700

    refactor: refactored registerTeam

--- src/services/competition.service.mjs
+++ src/services/competition.service.mjs
@@ -2,26 +2,26 @@ import prisma from "../prisma.mjs";
 import crypto from "crypto";
 
 export const registerTeam = async ({ competition_id, team_name }) => {
-    const random_id = crypto.randomBytes(32).toString("hex");
+    const random_id = crypto.randomUUID();
     const team_code = crypto.randomBytes(6).toString("base64url");
-    if (!prisma.competition.findUnique({ where: { id: competition_id } }))
-        throw { status: 404, message: "competition_id not found" };
 
-    if (prisma.team.findFirst({ where: { team_name } }))
-        throw { status: 406, message: "Team name already existed" };
+    const competitionExists = await prisma.competition.findUnique({ where: { id: competition_id } });
+    if (!competitionExists) throw { status: 404, message: "competition_id not found" };
+
+    const teamExists = await prisma.team.findFirst({ where: { team_name } });
+    if (teamExists) throw { status: 406, message: "Team name already exists" };
 
     try {
-        const team = await prisma.$transaction(async prismaClient => {
-            return prismaClient.team.create({
-                data: {
-                    id: random_id,
-                    competition_id,
-                    team_name,
-                    team_code,
-                },
-            });
+        await prisma.team.create({
+            data: {
+                id: random_id,
+                competition_id,
+                team_name,
+                team_code,
+            },
         });
-    } catch (err) {
+        return { message: "Team successfully registered" };
+    } catch (error) {
         console.error("Registration error:", error);
         throw { status: 500, message: "Failed to register team" };
     }
