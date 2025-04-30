c5e81b50d74259c86432a600f0432d18c746a82d
Aldi Pramudya <llenndesu@gmail.com>
2025-04-30 23:57:19 +0700

    feat: implement join competition with team code on controller

--- src/controllers/competition.controller.mjs
+++ src/controllers/competition.controller.mjs
@@ -12,3 +12,16 @@ export const registerCompetition = async (req, res) => {
         res.status(err.status || 500).json({ error: err.message });
     }
 };
+
+export const joinCompetitionWithTeamCode = async (req,res) => {
+    try {
+        const user_id = req.user.id;
+        const result = await compService.memberJoinWithTeamCode({
+            ...req.body,
+            user_id,
+        });
+        res.status(201).json(result);
+    } catch (err) {
+        res.status(err.status || 500).json({ error: err.message });
+    }
+}
