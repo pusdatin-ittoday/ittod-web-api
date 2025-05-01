cf9a5b7a61dfd0ecc87abe1e52c5344f5456f5e5
Aldi Pramudya <llenndesu@gmail.com>
2025-05-01 16:22:52 +0700

    refactor: checkUserCompetitionLimit.mjs

---
+++ src/helpers/checkUserCompetitionLimit.mjs
@@ -0,0 +1,16 @@
+export async function checkUserCompetitionLimit(prismaClient, userId) {
+    const userCompetitionsCount = await prismaClient.team_member.groupBy({
+        by: ["team_id"],
+        where: { user_id: userId },
+        _count: {
+            team_id: true,
+        },
+    }).then(groups => new Set(groups.map(group => group.team_id)).size);
+
+    if (userCompetitionsCount >= 2) {
+        throw {
+            status: 403,
+            message: "You can only participate in at most 2 competitions",
+        };
+    }
+}
