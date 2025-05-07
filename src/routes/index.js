181af3543dea8cd7e442b25dff087ecfdf9091fe
gamer <ilhamgoesasi@gmail.com>
2025-05-07 18:33:33 +0700

    timeline api

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,14 @@ const { Router } = require("express");
 const authRouter = require("./auth.js");
 const compeRouter = require("./competition.js");
 const fileRouter = require("./file");
+const timelineRouter = require("./timeline");
 
 const router = Router();
 
 router.use(authRouter);
 router.use(compeRouter);
 router.use(fileRouter);
+router.use(timelineRouter);
 
 router.get("/", (req, res) => {
     res.status(200).send("OK");
