2c822696f41850cd44f8ea7a8592e762c3f63aa5
Aldi Pramudya <llenndesu@gmail.com>
2025-05-11 13:26:05 +0700

    feat: kerangka user service buat edituserprofile

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,2 +1,15 @@
 const prisma = require("../prisma.js");
 
+const editUserProfile = async ({
+    full_name,
+    birth_date,
+    phone_number,
+    jenis_kelamin,
+    id_line,
+    id_discord,
+    id_instagram,
+    pendidikan,
+    pendidikan_lainnya,
+    nama_sekolah,
+    ktm,
+}) => {};
