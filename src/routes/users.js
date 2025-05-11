3117f959ff6f751c3d2e6db1647a993d4c77eced
Aldi Pramudya <llenndesu@gmail.com>
2025-05-11 12:49:16 +0700

    chore: emptying out the usernamechange deprecated function

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,19 +1,8 @@
-// @deprecated
-
 const { Router } = require("express");
 const { changeUsername } = require("../services/user.service.js");
 const { validateChangeUsername } = require("../middleware/userMiddleware.js");
 
 const usersRouter = Router();
 
-usersRouter.post(
-    "/api/user/change-username",
-    validateChangeUsername,
-    async (req, res) => {
-        const { oldUsername, newUsername } = req.body;
-        const result = await changeUsername(oldUsername, newUsername);
-        res.status(result.success ? 200 : 400).json(result);
-    }
-);
 
 module.exports = usersRouter;
