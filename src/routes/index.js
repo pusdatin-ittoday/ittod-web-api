29052c1f203d7a0a25ed7310930533216b5fa076
Aldi Pramudya <llenndesu@gmail.com>
2025-05-12 20:34:40 +0700

    feat: implemented upload images (IMAGE ONLY)

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,14 @@ const { Router } = require("express");
 const authRouter = require("./auth.js");
 const compeRouter = require("./competition.js");
 const fileRouter = require("./file");
+const uploadRouter = require("./upload");
 
 const router = Router();
 
 router.use(authRouter);
 router.use(compeRouter);
 router.use(fileRouter);
+router.use(uploadRouter);
 
 router.get("/", (req, res) => {
     res.status(200).send("OK");
