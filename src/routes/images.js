1d3daa8989d1d3a159a565e522ab1aaa191c5c8e
Aldi Pramudya <llenndesu@gmail.com>
2025-05-13 18:02:34 +0700

    feat: image getter validator from r2 to prevent abuse

--- src/routes/images.js
+++ src/routes/images.js
@@ -7,12 +7,15 @@ const {
 const images = multer({ storage: multer.memoryStorage() }); // Use memory storage for Buffer
 const uploadRouter = Router();
 const { isAuthenticated } = require("../middleware/authMiddleware.js");
-
+const { validateFile } = require("../middleware/imageValidator.js");
+const {getImageFromR2Validator} = require("../middleware/getImageFromR2Validator")
 uploadRouter.use(isAuthenticated);
-uploadRouter.get("/api/images/:key", getFileFromBucket);
+
+uploadRouter.get("/api/images/:key", getImageFromR2Validator, getFileFromBucket);
 
 uploadRouter.post(
     "/api/images/",
+    validateFile,
     images.single("image"),
     uploadFileToBucket
 );
