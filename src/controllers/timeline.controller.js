310a6dba78eaeaf489e3b286a93c8f80753089fe
Aldi Pramudya <llenndesu@gmail.com>
2025-05-14 11:31:12 +0700

    fix: only use prisma.js

--- src/controllers/timeline.controller.js
+++ src/controllers/timeline.controller.js
@@ -1,5 +1,4 @@
-const { PrismaClient } = require('../../prisma/app/generated/prisma/client');
-const prisma = new PrismaClient();
+const prisma = require("../prisma.js");
 
 exports.getAllTimelines = async (req, res) => {
     try {
