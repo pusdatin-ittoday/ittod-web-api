240337f6eacd64cf4739d84e9a895403c8b8531e
Aldi Pramudya <llenndesu@gmail.com>
2025-05-16 20:03:05 +0700

    fix: use prisma to check for admin

--- src/middleware/adminOnlyMiddleware.js
+++ src/middleware/adminOnlyMiddleware.js
@@ -1,9 +1,13 @@
+const prisma = require("../prisma")
 
-const checkAdmin = (req,res,next) => {
-    if (req.user.role !== "admin") {
+const checkAdmin = async (req, res, next) => {
+    const user = await prisma.user_identity.findUnique({
+        where: { id: req.user.id }
+    });
+    const role = user?.role;
+    if (role !== "admin") {
         return res.status(403).json({ message: "Admin Only" });
     }
     next();
 }
-
 module.exports = {checkAdmin}
