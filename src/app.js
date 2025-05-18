f2554a4a114bce9515bf7875dbdb12f79cb33db8
gamer <ilhamgoesasi@gmail.com>
2025-05-18 17:48:18 +0700

    enable CORS for test origins (localhost)

--- src/app.js
+++ src/app.js
@@ -3,12 +3,21 @@ const { json } = require("express");
 const session = require("express-session");
 const passport = require("passport");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 require("@dotenvx/dotenvx/config");
 
 const routes = require("./routes/index.js");
 const sessionConfig = require("./config/session.config.js");
 
 const app = express();
+
+// CORS configuration
+app.use(cors({
+    origin: ['http://localhost:3000', 'http://localhost:5173', 'http://127.0.0.1:3000'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 //middlewares
 app.use(json());
