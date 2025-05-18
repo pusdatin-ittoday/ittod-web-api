65fa9d4a18cc733d3c3c175f992727c4364c0455
gamer <ilhamgoesasi@gmail.com>
2025-05-18 13:18:12 +0700

    fix

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,11 +6,8 @@ const timelineRouter = require("./timeline");
 const uploadRouter = require("./images");
 const usersRouter = require("./user");
 const adminRouter = require("./admin");
-<<<<<<< HEAD
 const fetchHalamanKompetisiEventRouter = require("./fetch-halaman-kompetisi-event.js");
-=======
 const announcementRouter = require("./announcement.js");
->>>>>>> 89b99ec52ab117d9615cdc6df7159cf9a6af59db
 
 const router = Router();
 
@@ -24,10 +21,7 @@ router.use(timelineRouter);
 router.use(uploadRouter);
 router.use(usersRouter);
 router.use(adminRouter);
-<<<<<<< HEAD
 router.use(fetchHalamanKompetisiEventRouter);
-=======
 router.use(announcementRouter);
->>>>>>> 89b99ec52ab117d9615cdc6df7159cf9a6af59db
 
 module.exports = router;
