ec93dd03d2a8f1997cbd3ec707efe5592580ca77
Aldi Pramudya <llenndesu@gmail.com>
2025-05-19 15:24:33 +0700

    feat: added put twibbon functionality to API endpoint

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,7 +3,7 @@ const { editUserProfileController } = require("../controllers/user.controller");
 const { isAuthenticated } = require("../middleware/authMiddleware.js");
 const { validateUserProfile } = require("../middleware/userDataMiddleware");
 const { validateFile } = require("../middleware/imageValidator");
-
+const { putTwibbonUser } = require("../controllers/put-twibbon.controller")
 const multer = require("multer");
 
 const images = multer({
@@ -21,6 +21,14 @@ usersRouter.patch(
     validateFile,
     validateUserProfile,
     editUserProfileController
+);
+
+usersRouter.put(
+    "/api/user/twibbon",
+    isAuthenticated,
+    images.single("image"),
+    validateFile,
+    putTwibbonUser
 );
 
 module.exports = usersRouter;
