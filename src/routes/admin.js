8e3449f38926eb3bb8c4776d8f08262d273bab45
Aldi Pramudya <llenndesu@gmail.com>
2025-05-20 13:11:13 +0700

    feat: passport implemented

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -2,8 +2,21 @@ const { Router } = require("express");
 const { checkAdmin } = require("../middleware/adminOnlyMiddleware");
 const { syncKtm } = require("../controllers/sync-ktm.controller");
 const { isAuthenticated } = require("../middleware/authMiddleware.js");
+const passport = require("../strategies/admin-strategy");
+const { validateLogin, loginLimiter } = require("../middleware/authMiddleware");
+const passportAuthMiddleware = require("../middleware/passportAuthMiddleware");
+const { login } = require("../controllers/auth.controller");
+const preventLoginIfAuthenticated = require("../middleware/preventLoginIfAuthenticated");
 
 const adminRouter = Router();
 adminRouter.get("/api/admin/sync-ktm", isAuthenticated, checkAdmin, syncKtm);
 
+adminRouter.post(
+    "/api/admin/login",
+    loginLimiter,
+    preventLoginIfAuthenticated,
+    validateLogin,
+    passportAuthMiddleware("local"),
+    login
+);
 module.exports = adminRouter;
