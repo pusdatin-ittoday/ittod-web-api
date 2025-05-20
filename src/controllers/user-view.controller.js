4cbf18c6d62826c15d4bbeafa5b320c4929003b1
Aldi Pramudya <llenndesu@gmail.com>
2025-05-20 14:04:53 +0700

    fix: better error code for viewuserdata

--- src/controllers/user-view.controller.js
+++ src/controllers/user-view.controller.js
@@ -1,16 +1,18 @@
 const {viewUserDataService} = require("../services/user.service")
 
-const viewUserData = async (req,res) => {
+const viewUserData = async (req, res) => {
     try {
         const user_id = req.user?.id; // Ensure `req.user` exists
         if (!user_id) {
             return res.status(400).json({ message: "User ID is missing" });
         }
-
         const userData = await viewUserDataService(user_id);
+        if (!userData) {
+            return res.status(404).json({ message: "User not found" });
+        }
         return res.status(200).json(userData);
     } catch (error) {
-        console.error("Edit User Profile Error:", error);
+        console.error("View User Data Error:", error);
         res.status(error.status || 500).json({ message: error.message });
     }
 }
