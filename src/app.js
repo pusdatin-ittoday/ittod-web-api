e0893bf57c303457a4435e0821d50e1d34e516d0
Aldi Pramudya <llenndesu@gmail.com>
2025-05-21 10:42:52 +0700

    chore: allow *

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const app = express();
 
 // CORS configuration
 app.use(cors({
-    origin: true,
+    origin: "*",
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
