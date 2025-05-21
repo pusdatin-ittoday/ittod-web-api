ed980ae1741544d0db976b2d25abf6529c415ae6
Aldi Pramudya <llenndesu@gmail.com>
2025-05-21 16:11:30 +0700

    feat: implement into eventRouter

---
+++ src/routes/event.js
@@ -0,0 +1,13 @@
+const { Router } = require("express");
+const { isAuthenticated } = require("../middleware/authMiddleware.js");
+const { eventJoinController } = require("../controllers/event-user.controller");
+const loginSchema = require("../validators/eventRegisterValidationSchema");
+const { validateRequest } = require("../middleware/joiMiddleware");
+const eventRouter = Router();
+
+eventRouter.post(
+    "/api/event/join",
+    isAuthenticated,
+    validateRequest(loginSchema),
+    eventJoinController
+);
