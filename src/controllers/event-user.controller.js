37cb4a35ac39ba424a18dae4b3816147fcd1db4a
Aldi Pramudya <llenndesu@gmail.com>
2025-05-21 16:20:33 +0700

    feat: added event show for user

--- src/controllers/event-user.controller.js
+++ src/controllers/event-user.controller.js
@@ -1,3 +1,4 @@
+const prisma = require("../prisma");
 const { registerUserIntoEvent } = require("../services/event.service");
 
 const eventJoinController = async (req, res) => {
@@ -17,6 +18,28 @@ const eventJoinController = async (req, res) => {
         console.error("Error registering user into event", err);
         res.status(err.status || 500).json({
             error: err.message || "Failed to register user into event",
+        });
+    }
+};
+
+eventShowController = async (req, res) => {
+    try {
+        const user_id = req.user.id;
+        const result = await prisma.event_participant.findMany({
+            where: {
+                user_id,
+            },
+            include: {
+                event: {
+                    select: { title: true },
+                },
+            },
+        });
+        res.status(200).json(result);
+    } catch (err) {
+        console.error("Error fetching user events", err);
+        res.status(err.status || 500).json({
+            error: err.message || "Failed to fetch user events",
         });
     }
 };
