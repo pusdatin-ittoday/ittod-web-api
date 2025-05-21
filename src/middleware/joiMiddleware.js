b70cffba9c7929c09fafc52929e26ece045a43ee
Aldi Pramudya <llenndesu@gmail.com>
2025-05-21 16:10:19 +0700

    feat: added middleware for joi

---
+++ src/middleware/joiMiddleware.js
@@ -0,0 +1,15 @@
+// noinspection JSUnusedLocalSymbols
+
+const Joi = require("joi");
+
+const validateRequest = schema => (req, res, next) => {
+    const { error } = schema.validate(req.body, { abortEarly: false });
+    if (error) {
+        return res.status(400).json({
+            errors: error.details.map(err => err.message),
+        });
+    }
+    next();
+};
+
+module.exports = { validateRequest };
