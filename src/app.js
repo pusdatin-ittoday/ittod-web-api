aefb526237a82302d5c728a5e534fcf768872833
Aldi Pramudya <llenndesu@gmail.com>
2025-05-22 13:48:52 +0700

    fix: cors allow all

--- src/app.js
+++ src/app.js
@@ -3,14 +3,22 @@ const { json } = require("express");
 const session = require("express-session");
 const passport = require("passport");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
 require("@dotenvx/dotenvx/config");
 
 const routes = require("./routes/index.js");
 const sessionConfig = require("./config/session.config.js");
 
 const app = express();
-
+const corsOptions = {
+    origin: "*", // Adjust this to your needs
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    optionsSuccessStatus: 200,
+};
 //middlewares
+app.use(cors(corsOptions));
 app.use(json());
 app.use(cookieParser(process.env.SECRET_KEY_SESSION));
 app.use(session(sessionConfig));
