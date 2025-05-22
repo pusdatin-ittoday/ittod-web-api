620522b4e35379b2fae7a76cd33094dea3dfa4ff
Aldi Pramudya <llenndesu@gmail.com>
2025-05-22 14:10:20 +0700

    fix: cors allow all localhost

--- src/app.js
+++ src/app.js
@@ -12,15 +12,10 @@ const sessionConfig = require("./config/session.config.js");
 
 const app = express();
 const corsOptions = {
-    origin: (origin, callback) => {
-        if (!origin || /^(http:\/\/localhost(:\d+)?)/.test(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
+    origin: ["http://localhost:5173", "https://staging.ittoday.web.id"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true, // Allow credentials
     optionsSuccessStatus: 200,
 };
 //middlewares
