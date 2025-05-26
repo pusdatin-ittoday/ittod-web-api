25a7e7e54897571794501d641fd81f949656df56
walidan-nadilaw <sirsnakeson@gmail.com>
2025-05-26 18:25:37 +0700

    create admin.services.js

---
+++ src/services/admin.services.js
@@ -0,0 +1,59 @@
+const prisma = require("../prisma.js");
+
+const getCompetitionById = async(id) => {
+    try{
+        const competition = await prisma.event.findFirst({
+            where: {
+                id: parseInt(id),
+                type: 'competition'
+            },
+            select: {
+                id: true,
+                title: true,
+                description: true,
+                guide_book_url: true,
+                contact_person1: true,
+                contact_person2: true,
+                timeline: {
+                    select: {
+                        id: true,
+                        title: true,
+                        date: true
+                    },
+                    orderBy: {
+                        date: 'asc'
+                    }
+                }
+            }
+        });
+
+        if (!competition) {
+            throw {
+                status: 404,
+                message: `competition id: ${id} not found`
+            };
+        } 
+
+        return {
+            id: competition.id,
+            title: competition.title,
+            description: competition.description,
+            guide_book_url: competition.guide_book_url,
+            contact_person1: competition.contact_person1,
+            contact_person2: competition.contact_person2,
+            timeline: competition.timeline
+        };
+
+    } catch (error) {
+        
+        if (error.status) {
+            throw error;
+        }
+
+        throw {
+            status: 500,
+            message: 'Internal Server Error',
+            detail : error.message || error
+        };
+    }
+}
