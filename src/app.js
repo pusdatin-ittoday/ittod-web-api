30579a615650d292284dd4f81da0e776306cdea4
Aldi Pramudya <llenndesu@gmail.com>
2025-05-27 03:16:20 +0700

    fix: cookie fix 10

--- src/app.js
+++ src/app.js
@@ -11,12 +11,13 @@ const routes = require("./routes/index.js");
 const sessionConfig = require("./config/session.config.js");
 
 const app = express();
-const corsOptions = {
-    origin: ["https://ittoday.web.id"],
-    credentials: true,
-};
 //middlewares
-app.use(cors(corsOptions));
+app.use(
+    cors({
+        origin: ["https://ittoday.web.id"],
+        credentials: true,
+    })
+);
 app.use(json());
 app.use(cookieParser(process.env.SECRET_KEY_SESSION));
 app.use(session(sessionConfig));
