ba5a237b104f6687a29f1394e4aca0e18be950d4
Aldi Pramudya <llenndesu@gmail.com>
2025-05-27 00:19:20 +0700

    feat: initiate database

---
+++ src/controllers/initate-db.controller.js
@@ -0,0 +1,21 @@
+const prisma = require("../prisma");
+
+const events = [{}, {}, {}];
+const timelines = [{}, {}, {}];
+const initiateDatabase = async (req, res) => {
+    const eventResult = await prisma.event.createMany({
+        data: events,
+    });
+    const timelineResult = await prisma.event_timeline.createMany({
+        data: timelines,
+    });
+
+    const results = {
+        eventResult,
+        timelineResult,
+        message: "dont run this ever again bro...",
+    };
+    res.json(results);
+};
+
+module.exports = { initiateDatabase };
