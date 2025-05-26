9f35287c4c2a7f8beac80b81a9a0187f4f5dba48
Aldi Pramudya <llenndesu@gmail.com>
2025-05-27 02:07:33 +0700

    fix: ilham...

--- src/config/session.config.js
+++ src/config/session.config.js
@@ -13,6 +13,7 @@ module.exports = {
     cookie: {
         secure: true,
         sameSite: "None",
+        domain: process.env.COOKIE_DOMAIN,
         httpOnly: false,
         maxAge: 1000 * 60 * 60 * 24,
     },
