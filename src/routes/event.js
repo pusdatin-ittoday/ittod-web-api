a6503d4163668e9680a2b027346e55adf7972bf9
Aldi Pramudya <llenndesu@gmail.com>
2025-07-18 14:41:28 +0700

    feat: bootcamp controller is now exist

--- src/routes/event.js
+++ src/routes/event.js
@@ -3,9 +3,11 @@ const { isAuthenticated } = require("../middleware/authMiddleware.js");
 const {
     eventJoinController,
     eventShowController,
+    bootcampRegistrationController,
 } = require("../controllers/event-user.controller");
 const eventRegisterSchema = require("../validators/eventRegisterValidationSchema");
 const { validateRequest } = require("../middleware/joiMiddleware");
+const bootcampRegisterSchema = require("../validators/bootcampRegistrationSchema");
 const eventRouter = Router();
 
 eventRouter.post(
@@ -13,6 +15,13 @@ eventRouter.post(
     isAuthenticated,
     validateRequest(eventRegisterSchema),
     eventJoinController
+);
+
+eventRouter.post(
+    "/api/event/bootcamp/join",
+    isAuthenticated,
+    validateRequest(bootcampRegisterSchema),
+    bootcampRegistrationController
 );
 
 eventRouter.get("/api/event/", isAuthenticated, eventShowController);
